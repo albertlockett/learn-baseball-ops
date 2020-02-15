0f90ed6f81b246c8f0c77607e8a4914d92eeb6d3
albertlockett <albert.lockett@gmail.com>
2020-02-15 00:12:40 -0400

    scripts to bootstrap database

--- scripts/configure-docker.sh
+++ scripts/configure-docker.sh
@@ -2,3 +2,4 @@
 ACCESS_KEY=$(aws secretsmanager --region us-east-1 get-secret-value --secret-id dockerhub_access_token | jq '.SecretString | fromjson | .dockerhub_api_key' | sed -e 's/"//g')
 docker login --username albertlockett2 --password $ACCESS_KEY
 docker pull albertlockett2/learnbaseball-graphql-api:latest
+docker pull albertlockett2/learnbaseball-db:latest
--- scripts/provision.sh
+++ scripts/provision.sh
@@ -3,8 +3,11 @@
 "exec ./scripts/configure-docker.sh"
 
 echo "create cronjob to run graphql-server"
-
 crontab -l > mycron
 echo "* * * * * bash -c $(pwd)/scripts/is-graphql-running.sh" >> mycron
 crontab mycron
 rm mycron
+
+# boostrap ES data
+echo "Boostrapping DB data: " + $ESHOST
+docker run albertlockett2/learnbaeball-db:latest /app/main --eshost $ESHOST
